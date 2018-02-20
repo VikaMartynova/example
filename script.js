928f7c29762a31e74682f2aafece2b6208e44c9c
Viktoria Martynova <vikam1210@gmail.com>
2018-02-20 15:55:06 +0200

    Add gallery

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// window.onload = function(){
+// 	console.log("1");
+// // var btnPrev = document.querySelector('#gallery .buttons .prev');
+// // var btnNext = document.querySelector('#gallery .buttons .next');
+
+// // var images = document.querySelectorAll('#gallery .photos img');
+
+// btnPrev.onclick = function(){
+// 	alert("Say");
+// }
+
+// }
+alert(1);
