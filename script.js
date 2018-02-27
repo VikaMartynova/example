379df524072bc31b06fe7f1e5eee23e42be4e61e
Viktoria Martynova <vikam1210@gmail.com>
2018-02-27 16:59:16 +0200

    Add gallery

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 
-// window.onload = function(){
-// 	console.log("1");
-// // var btnPrev = document.querySelector('#gallery .buttons .prev');
-// // var btnNext = document.querySelector('#gallery .buttons .next');
+var btnPrev = document.querySelector('#gallery .buttons .prev');
+var btnNext = document.querySelector('#gallery .buttons .next');
 
-// // var images = document.querySelectorAll('#gallery .photos img');
+var images = document.querySelectorAll('#gallery .photos img');
+var i = 0;
+console.log(-9%10);
 
-// btnPrev.onclick = function(){
-// 	alert("Say");
-// }
+btnNext.onclick = function(){
+	images[i++].className = '';
+	i = i%images.length;
+	images[i].className = 'showed';
+}
+btnPrev.onclick = function(){
+	images[i--].className = '';
+	var it =i%images.length;
+	i = it < 0 ? it+images.length : it;
+	images[i].className = 'showed';
+}
 
-// }
-alert(1);
+
